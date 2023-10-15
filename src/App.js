e220bf885d884d314a30be01899c3ea69dd8e162
anill-2022 <anilchirla1@gmail.com>
2023-10-15 11:46:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // Import your CSS file
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="portfolio">
+      <header>
+        <h1>ANIL CHIRLA</h1>
+        <p>IoT Specialist,Web Developer</p>
       </header>
+      <section className="projects">
+        <h2>Projects</h2>
+        <b>Related to SQL and python</b>
+      </section>
+      <section className="skills">
+        <h2>Skills</h2>
+        <b>Python,C++,HTML,CSS,JavaScript,ReactJS,MongoDB
+        </b>
+      </section>
+      <section className="experience">
+        <h2>Experience</h2>
+        <b>Worked as Core Member in VITMAS Club</b>
+      </section>
+      <section className="education">
+        <h2>Education</h2>
+        <b>B.Tech with specilization in Internet of Things</b>
+      </section>
+      <section className="contact">
+        <h2>Contact</h2>
+        <b>Email:anilchirla1@gmail.com</b>
+      </section>
     </div>
   );
-}
+};
 
 export default App;
